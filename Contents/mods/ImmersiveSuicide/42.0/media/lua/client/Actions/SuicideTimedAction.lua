4af1687d227ae6a97924dfe8a5d58791f559e656
Kyle Stankovich <stankawake@gmail.com>
2025-02-20 16:27:43 -0600

    Changed name to 'Immersive Suicide'. Updated code and translations to use new name. Fixed bug where single player muzzle flash wouldn't player

---
+++ Contents/mods/ImmersiveSuicide/42.0/media/lua/client/Actions/SuicideTimedAction.lua
@@ -0,0 +1,175 @@
+require "TimedActions/ISBaseTimedAction"
+
+SuicideTimedAction = ISBaseTimedAction:derive("SuicideTimedAction")
+
+function SuicideTimedAction:isValid()
+    return true
+end
+
+function SuicideTimedAction:start()
+    if not self.anim then
+        print("[Immersive Suicide] No animation for suicide set, forcing complete")
+        self:forceComplete()
+        return
+    end
+
+    if not self.weapon then
+        print("[Immersive Suicide] No weapon for suicide set, forcing complete")
+        self:forceComplete()
+        return
+    end
+
+    self:setActionAnim(self.anim)
+end
+
+function SuicideTimedAction:update()
+    -- Ensure game speed is always running at 1 during action
+    local gameSpeed = UIManager:getSpeedControls():getCurrentGameSpeed()
+    if gameSpeed ~= 1 then
+        UIManager:getSpeedControls():SetCurrentGameSpeed(1)
+    end
+end
+
+-- Ripped from ISReloadWeaponAction.attackHook
+local function playWeaponEffects(character, weapon)
+    character:playSound(weapon:getSwingSound());
+
+    local radius = weapon:getSoundRadius();
+    if isClient() then -- limit sound radius in MP
+        radius = radius / 1.8;
+    end
+
+    character:addWorldSoundUnlessInvisible(radius, weapon:getSoundVolume(), false);
+end
+
+-- Ripped from ISReloadWeaponAction.onShoot
+local function processWeaponAmmo(character, weapon)
+    if weapon:haveChamber() then
+		weapon:setRoundChambered(false);
+	end
+
+	if weapon:isRackAfterShoot() then
+		-- shotgun need to be rack after each shot to rechamber round
+		-- See ISReloadWeaponAction.OnPlayerAttackFinished()
+		if weapon:haveChamber() then
+			weapon:setSpentRoundChambered(true);
+		end
+	else
+		-- automatic weapons eject the bullet cartridge
+		if not weapon:isManuallyRemoveSpentRounds() then
+			if weapon:getShellFallSound() then
+                character:playSound(weapon:getShellFallSound())
+			end
+		end
+
+		if weapon:getCurrentAmmoCount() >= weapon:getAmmoPerShoot() then
+			-- remove ammo, add one to chamber if we still have some
+			if weapon:haveChamber() then
+				weapon:setRoundChambered(true);
+			end
+			weapon:setCurrentAmmoCount(weapon:getCurrentAmmoCount() - weapon:getAmmoPerShoot())
+		end
+	end
+
+	if weapon:isManuallyRemoveSpentRounds() then
+		weapon:setSpentRoundCount(weapon:getSpentRoundCount() + weapon:getAmmoPerShoot())
+	end
+end
+
+local function syncCharacterFX(character)
+    character:startMuzzleFlash()
+    character:splatBloodFloorBig()
+end
+
+local function performSuicide(character)
+    character:Kill(character)
+end
+
+local function killCharacter(character)
+    local body = character:getBodyDamage()
+    local head = body:getBodyPart(BodyPartType.Head)
+
+    head:setHaveBullet(true, 0)
+    head:SetHealth(0)
+
+    -- Force zombie if sandbox settings are enabled for it
+    -- Otherwise, prevent zombie (even if bitten) because the head just got annihilated
+    if SandboxVars.ImmersiveSuicide.ForceZombification == true then
+        body:setInfected(true)
+        body:setInfectionMortalityDuration(0)
+        body:setInfectionLevel(100)
+    else
+        body:setInfected(false)
+        body:setInfectionMortalityDuration(-1)
+        body:setInfectionLevel(0)
+    end
+    
+    if isClient() then
+        sendClientCommand("ImmersiveSuicide", "requestPerformSuicide", {})
+    else
+        performSuicide(character)
+    end
+end
+
+function SuicideTimedAction:shootWeapon()
+    playWeaponEffects(self.character, self.weapon)
+    processWeaponAmmo(self.character, self.weapon)
+    if isClient() then
+        sendClientCommand("ImmersiveSuicide", "requestSynchronizedCharacterFX", {})
+    else
+        syncCharacterFX(self.character)
+    end
+end
+
+function SuicideTimedAction:animEvent(event, parameter)
+    if event == "shootTime" then
+        self:shootWeapon()
+    end
+
+    if event == "killTime" then
+        killCharacter(self.character)
+        self:forceComplete()
+    end
+end
+
+function SuicideTimedAction:perform()
+    ISBaseTimedAction.perform(self)
+end
+
+function SuicideTimedAction:new(character, weapon, anim, maxTime)
+    local obj = ISBaseTimedAction:new(character)
+    setmetatable(obj, self)
+    self.__index = self
+
+    obj.stopOnAim = true
+    obj.stopOnWalk = true
+    obj.stopOnRun = true
+    
+    if maxTime == nil then
+        obj.maxTime = -1
+    else
+        obj.maxTime = maxTime
+    end
+
+    obj.anim = anim
+    obj.weapon = weapon
+
+    return obj
+end
+
+local function onSuicideServerCommand(module, command, args)
+    if module ~= "ImmersiveSuicide" then
+        return
+    end
+
+    if command == "recieveSynchronizedCharacterFX" then
+        local character = getPlayerByOnlineID(args.playerOnlineId)
+        syncCharacterFX(character)
+    end
+
+    if command == "recievePerformSuicide" then
+        local character = getPlayerByOnlineID(args.playerOnlineId)
+        performSuicide(character)
+    end
+end
+Events.OnServerCommand.Add(onSuicideServerCommand)
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Stank's Suicide
+# Immersive Suicide
 
 Simple Project Zomboid mod that adds the option to end an injured character's suffering. Works in Build 42 and Build 41.
 
